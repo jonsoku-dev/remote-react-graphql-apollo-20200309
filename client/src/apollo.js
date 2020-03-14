bc254e776b97b0d705708ef17062076ce1a97303
jonsoku2 <whdtjr2792@gmail.com>
2020-03-14 10:14:32 +0900

    heroku

--- client/src/apollo.js
+++ client/src/apollo.js
@@ -1,8 +1,8 @@
 import ApolloClient from "apollo-boost";
 
 export const apollo = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
-  // uri: "https://tamastudy-remote.herokuapp.com/graphql",
+  // uri: "http://localhost:4000/graphql",
+  uri: "https://tamastudy-remote.herokuapp.com/graphql",
   fetchOptions: {
     credentials: "include"
   },
